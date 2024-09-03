fc2ef035802c414cf7a9f072cc3ef70fc9635375
BuickSalesman <adam.odonnell.e@gmail.com>
2024-09-03 18:25:08 -0400

    added some code to draw a rectangle

--- movement-test/movement-test-script.js
+++ movement-test/movement-test-script.js
@@ -11,9 +11,17 @@ function resizeCanvas() {
 
 resizeCanvas();
 
+canvas.addEventListener("mouseenter", () => {
+  canvas.classList.add("custom-cursor"); // Apply the custom cursor class
+});
+
+canvas.addEventListener("mouseleave", () => {
+  canvas.classList.remove("custom-cursor"); // Remove the custom cursor class
+});
+
 //define max movement distance
 
-//define box -- x, y, width, height, velocity(vx, vy?), shooting range(invisible)
+//define box -- x, y, width, height, shooting range(invisible)
 //make box -- line by line animation would be fun
 
 //listen for click on box
@@ -32,3 +40,45 @@ resizeCanvas();
 //show last three turns of movement/shooting, with a general degradation as time goes by
 
 //will have to add collision conditionals later obviously
+
+const startX = 450; // X coordinate of the top-left corner
+const startY = 900; // Y coordinate of the top-left corner
+const width = 50; // Width of the rectangle
+const height = 50; // Height of the rectangle
+
+function drawLeftLine() {
+  ctx.beginPath();
+  ctx.moveTo(startX, startY);
+  ctx.lineTo(startX, startY + height);
+  ctx.stroke();
+}
+
+function drawTopLine() {
+  ctx.beginPath();
+  ctx.moveTo(startX, startY + height);
+  ctx.lineTo(startX + width, startY + height);
+  ctx.stroke();
+}
+
+function drawRightLine() {
+  ctx.beginPath();
+  ctx.moveTo(startX + width, startY + height);
+  ctx.lineTo(startX + width, startY);
+  ctx.stroke();
+}
+
+function drawBottomLine() {
+  ctx.beginPath();
+  ctx.moveTo(startX + width, startY);
+  ctx.lineTo(startX, startY);
+  ctx.stroke();
+}
+
+function drawBoxObject() {
+  setTimeout(drawLeftLine, 250); // Draw left line after 0.25 seconds
+  setTimeout(drawTopLine, 500); // Draw top line after 0.5 seconds
+  setTimeout(drawRightLine, 750); // Draw right line after 0.75 seconds
+  setTimeout(drawBottomLine, 1000); // Draw bottom line after 1 second
+}
+
+drawBoxObject();
